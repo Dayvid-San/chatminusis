53347bb1d52fcaf780c91a38350789b69e1d9756
Dayvid Santana da Silva <77745454+Dayvid-San@users.noreply.github.com>
2026-03-09 11:53:01 -0400

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,36 +1,47 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) return web;
+    if (kIsWeb) {
+      return web;
+    }
     switch (defaultTargetPlatform) {
-      case TargetPlatform.android: return android;
-      case TargetPlatform.iOS: return ios;
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
       default:
-        throw UnsupportedError('Plataforma não suportada');
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
     }
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'SUA-CHAVE-AQUI',
-    appId: 'SUA-ID-AQUI',
-    messagingSenderId: '000000000',
-    projectId: 'seu-projeto-id',
+    apiKey: 'AIzaSyAiXkAneOkKOoiZ5nl79c-byFi2CefUsDo',
+    appId: '1:216172016070:android:7d7dc1260d1c0389e1c2c7', // Usando ID compatível
+    messagingSenderId: '216172016070',
+    projectId: 'chatminusis',
+    authDomain: 'chatminusis.firebaseapp.com',
+    storageBucket: 'chatminusis.firebasestorage.app',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'SUA-CHAVE-AQUI',
-    appId: 'SUA-ID-AQUI',
-    messagingSenderId: '000000000',
-    projectId: 'seu-projeto-id',
+    apiKey: 'AIzaSyAiXkAneOkKOoiZ5nl79c-byFi2CefUsDo',
+    appId: '1:216172016070:android:7d7dc1260d1c0389e1c2c7',
+    messagingSenderId: '216172016070',
+    projectId: 'chatminusis',
+    storageBucket: 'chatminusis.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'SUA-CHAVE-AQUI',
-    appId: 'SUA-ID-AQUI',
-    messagingSenderId: '000000000',
-    projectId: 'seu-projeto-id',
-    iosBundleId: 'com.exemplo.myapp',
+    apiKey: 'AIzaSyAiXkAneOkKOoiZ5nl79c-byFi2CefUsDo',
+    appId: '1:216172016070:android:7d7dc1260d1c0389e1c2c7',
+    messagingSenderId: '216172016070',
+    projectId: 'chatminusis',
+    storageBucket: 'chatminusis.firebasestorage.app',
+    iosBundleId: 'com.chatminusis.app',
   );
 }
